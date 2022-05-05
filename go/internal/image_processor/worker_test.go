76b45bb7571c11e83b6f4e4522b310a0c0cbcd85
TroyKomodo <troybensonsa@gmail.com>
2022-05-05 03:55:07 -0400

    better testing styles

--- go/internal/image_processor/worker_test.go
+++ go/internal/image_processor/worker_test.go
@@ -2,8 +2,10 @@ package image_processor
 
 import (
 	"context"
+	"fmt"
 	"path"
 	"runtime"
+	"sync"
 	"testing"
 
 	"github.com/seventv/image-processor/go/internal/configure"
@@ -14,8 +16,32 @@ import (
 	"github.com/seventv/image-processor/go/internal/testutil"
 )
 
-func setup(t *testing.T, gCtx global.Context, files ...string) {
+var assets = []string{
+	// "animated-1.avif",
+	"animated-1.gif",
+	"animated-1.png",
+	"animated-1.webp",
+	"animated-2.gif",
+	"animated-2.webp",
+	"animated-3.gif",
+	"animated.avi",
+	"animated.flv",
+	"animated.mov",
+	"animated.mp4",
+	"animated.webm",
+	// "static-1.avif",
+	"static-1.jpeg",
+	"static-1.png",
+	"static-1.tiff",
+	"static-1.webp",
+	// "static-2.avif",
+	"static-2.webp",
+}
+
+func TestWorker(t *testing.T) {
 	var err error
+	gCtx, cancel := global.WithCancel(global.New(context.Background(), &configure.Config{}))
+
 	gCtx.Inst().KubeMQ, err = kubemq.NewMock(gCtx)
 	testutil.IsNil(t, err, "kubemq init successful")
 	gCtx.Inst().Prometheus = prometheus.New(prometheus.Options{})
@@ -28,213 +54,45 @@ func setup(t *testing.T, gCtx global.Context, files ...string) {
 		"output": {},
 	}
 
-	for _, file := range files {
+	for _, file := range assets {
 		f["input"][file] = testutil.ReadFile(t, path.Join(assetDir, file))
 	}
 
 	gCtx.Inst().S3, err = s3.NewMock(gCtx, f)
 	testutil.IsNil(t, err, "s3 init successful")
-}
 
-// func TestAnimatedAvif1(t *testing.T) {
-// 	var err error
-// 	gCtx, cancel := global.WithCancel(global.New(context.Background(), &configure.Config{}))
-// 	defer cancel()
-// 	setup(t, gCtx, "animated-1.avif")
+	wg := sync.WaitGroup{}
+	wg.Add(len(assets))
 
-// 	testutil.IsNil(t, err, "s3 init successful")
+	for _, file := range assets {
+		file := file
+		t.Run(fmt.Sprintf("test %s", file), func(t *testing.T) {
+			t.Parallel()
 
-// 	Run(gCtx)
+			defer wg.Done()
 
-// 	worker := Worker{}
-// 	result := Result{}
-// 	err = worker.Work(gCtx, Task{
-// 		Flags: TaskFlagALL,
-// 		Input: TaskInput{
-// 			Bucket: "input",
-// 			Key:    "animated-1.avif",
-// 		},
-// 		Output: TaskOutput{
-// 			Bucket: "output",
-// 			Prefix: "",
-// 		},
-// 		SmallestMaxWidth:  96,
-// 		SmallestMaxHeight: 32,
-// 		Scales:            []int{1, 2, 3, 4},
-// 	}, &result)
-// 	testutil.IsNil(t, err, "Convert was successful")
-// }
+			worker := Worker{}
+			result := Result{}
+			err := worker.Work(gCtx, Task{
+				Flags: TaskFlagALL,
+				Input: TaskInput{
+					Bucket: "input",
+					Key:    file,
+				},
+				Output: TaskOutput{
+					Bucket: "output",
+					Prefix: file,
+				},
+				SmallestMaxWidth:  96,
+				SmallestMaxHeight: 32,
+				Scales:            []int{1, 2, 3, 4},
+			}, &result)
+			testutil.IsNil(t, err, "Convert was successful")
+		})
+	}
 
-func TestAnimatedGif1(t *testing.T) {
-	var err error
-	gCtx, cancel := global.WithCancel(global.New(context.Background(), &configure.Config{}))
-	defer cancel()
-	setup(t, gCtx, "animated-1.gif")
-
-	testutil.IsNil(t, err, "s3 init successful")
-
-	Run(gCtx)
-
-	worker := Worker{}
-	result := Result{}
-	err = worker.Work(gCtx, Task{
-		Flags: TaskFlagALL,
-		Input: TaskInput{
-			Bucket: "input",
-			Key:    "animated-1.gif",
-		},
-		Output: TaskOutput{
-			Bucket: "output",
-			Prefix: "",
-		},
-		SmallestMaxWidth:  96,
-		SmallestMaxHeight: 32,
-		Scales:            []int{1, 2, 3, 4},
-	}, &result)
-	testutil.IsNil(t, err, "Convert was successful")
-}
-
-func TestAnimatedPng1(t *testing.T) {
-	var err error
-	gCtx, cancel := global.WithCancel(global.New(context.Background(), &configure.Config{}))
-	defer cancel()
-	setup(t, gCtx, "animated-1.png")
-
-	testutil.IsNil(t, err, "s3 init successful")
-
-	Run(gCtx)
-
-	worker := Worker{}
-	result := Result{}
-	err = worker.Work(gCtx, Task{
-		Flags: TaskFlagALL,
-		Input: TaskInput{
-			Bucket: "input",
-			Key:    "animated-1.png",
-		},
-		Output: TaskOutput{
-			Bucket: "output",
-			Prefix: "",
-		},
-		SmallestMaxWidth:  96,
-		SmallestMaxHeight: 32,
-		Scales:            []int{1, 2, 3, 4},
-	}, &result)
-	testutil.IsNil(t, err, "Convert was successful")
-}
-
-func TestAnimatedWebp1(t *testing.T) {
-	var err error
-	gCtx, cancel := global.WithCancel(global.New(context.Background(), &configure.Config{}))
-	defer cancel()
-	setup(t, gCtx, "animated-1.webp")
-
-	testutil.IsNil(t, err, "s3 init successful")
-
-	Run(gCtx)
-
-	worker := Worker{}
-	result := Result{}
-	err = worker.Work(gCtx, Task{
-		Flags: TaskFlagALL,
-		Input: TaskInput{
-			Bucket: "input",
-			Key:    "animated-1.webp",
-		},
-		Output: TaskOutput{
-			Bucket: "output",
-			Prefix: "",
-		},
-		SmallestMaxWidth:  96,
-		SmallestMaxHeight: 32,
-		Scales:            []int{1, 2, 3, 4},
-	}, &result)
-	testutil.IsNil(t, err, "Convert was successful")
-}
-
-func TestAnimatedGif2(t *testing.T) {
-	var err error
-	gCtx, cancel := global.WithCancel(global.New(context.Background(), &configure.Config{}))
-	defer cancel()
-	setup(t, gCtx, "animated-2.gif")
-
-	testutil.IsNil(t, err, "s3 init successful")
-
-	Run(gCtx)
-
-	worker := Worker{}
-	result := Result{}
-	err = worker.Work(gCtx, Task{
-		Flags: TaskFlagALL,
-		Input: TaskInput{
-			Bucket: "input",
-			Key:    "animated-2.gif",
-		},
-		Output: TaskOutput{
-			Bucket: "output",
-			Prefix: "",
-		},
-		SmallestMaxWidth:  96,
-		SmallestMaxHeight: 32,
-		Scales:            []int{1, 2, 3, 4},
-	}, &result)
-	testutil.IsNil(t, err, "Convert was successful")
-}
-
-func TestAnimatedWebp2(t *testing.T) {
-	var err error
-	gCtx, cancel := global.WithCancel(global.New(context.Background(), &configure.Config{}))
-	defer cancel()
-	setup(t, gCtx, "animated-2.webp")
-
-	testutil.IsNil(t, err, "s3 init successful")
-
-	Run(gCtx)
-
-	worker := Worker{}
-	result := Result{}
-	err = worker.Work(gCtx, Task{
-		Flags: TaskFlagALL,
-		Input: TaskInput{
-			Bucket: "input",
-			Key:    "animated-2.webp",
-		},
-		Output: TaskOutput{
-			Bucket: "output",
-			Prefix: "",
-		},
-		SmallestMaxWidth:  96,
-		SmallestMaxHeight: 32,
-		Scales:            []int{1, 2, 3, 4},
-	}, &result)
-	testutil.IsNil(t, err, "Convert was successful")
-}
-
-func TestAnimatedGif3(t *testing.T) {
-	var err error
-	gCtx, cancel := global.WithCancel(global.New(context.Background(), &configure.Config{}))
-	defer cancel()
-	setup(t, gCtx, "animated-3.gif")
-
-	testutil.IsNil(t, err, "s3 init successful")
-
-	Run(gCtx)
-
-	worker := Worker{}
-	result := Result{}
-	err = worker.Work(gCtx, Task{
-		Flags: TaskFlagALL,
-		Input: TaskInput{
-			Bucket: "input",
-			Key:    "animated-3.gif",
-		},
-		Output: TaskOutput{
-			Bucket: "output",
-			Prefix: "",
-		},
-		SmallestMaxWidth:  96,
-		SmallestMaxHeight: 32,
-		Scales:            []int{1, 2, 3, 4},
-	}, &result)
-	testutil.IsNil(t, err, "Convert was successful")
+	go func() {
+		wg.Wait()
+		cancel()
+	}()
 }
