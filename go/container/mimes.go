bfd2b5d58a659b0df6b9cec4e268bc5a3ee03534
TroyKomodo <troybensonsa@gmail.com>
2022-06-19 17:57:55 -0400

    mimes

---
+++ go/container/mimes.go
@@ -0,0 +1,11 @@
+package container
+
+import "github.com/h2non/filetype/matchers"
+
+var (
+	MimeAVIF = TypeAvif.MIME.Value
+	MimeWEBP = matchers.TypeWebp.MIME.Value
+	MimeGIF  = matchers.TypeGif.MIME.Value
+	MimePNG  = matchers.TypePng.MIME.Value
+	MimeZIP  = matchers.TypeZip.MIME.Value
+)
